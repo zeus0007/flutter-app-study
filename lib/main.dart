07086384eaa57fd3aa6c80890c82f79e4ffd79f2
zeus <wpdn79@likelion.org>
2019-12-26 10:23:24 +0900

    image(1)

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: '연습',
-        theme: ThemeData(primarySwatch: Colors.blue),
-        home: MainPage());
+    return MaterialApp(title: '연습', home: MainPage());
   }
 }
 
@@ -18,30 +15,16 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('멀티 텍스트')),
-      body: Column(
-        children: <Widget>[
-          makeRow('1', '2', '3'),
-          makeRow('4', '5', '6'),
-          makeRow('7', '8', '9'),
-        ],
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      ),
-    );
+        appBar: AppBar(
+          title: Text('사진'),
+        ),
+        body: MyApp1());
   }
+}
 
-  Widget makeRow(String left, String middle, String right) {
-    return Row(
-        children: <Widget>[box(left), box(middle), box(right)],
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly);
-  }
-
-  Widget box(String number) {
-    return Container(
-      child: Center(child: Text(number)),
-      height: 100,
-      width: 100,
-      decoration: BoxDecoration(color: Colors.red),
-    );
+class MyApp1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset('images/test_img1.jpg');
   }
 }
