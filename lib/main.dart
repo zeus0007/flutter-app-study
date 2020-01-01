da8a5802de6171e381c7be212364603e89929f70
zeus <wpdn79@likelion.org>
2020-01-01 23:14:25 +0900

    탭바 기본

--- lib/main.dart
+++ lib/main.dart
@@ -12,41 +12,30 @@ class MyApp extends StatelessWidget {
 }
 
 class MainPage extends StatelessWidget {
+  final choices = ['PLANE', 'CAR', 'BIKE', 'BOAT', 'BUS', 'TRAIN', 'WALK'];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('첫 번째')),
-      body: Center(
-        child: RaisedButton(
-          child: Text('두 번째 화면으로 이동', style: TextStyle(fontSize: 21)),
-          color: Colors.blue,
-          onPressed: () {
-            Navigator.push(context,
-                MaterialPageRoute<void>(builder: (BuildContext context) {
-              return Second();
-            }));
-          },
-        ),
-      ),
-    );
-  }
-}
-
-class Second extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('두 번째')),
-      body: Center(
-        child: RaisedButton(
-          child: Text(
-            '첫 화면으로 돌아가기',
-            style: TextStyle(fontSize: 21),
+    return DefaultTabController(
+      length: choices.length,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('교통 수단'),
+          bottom: TabBar(
+            tabs: choices.map((String choice) {
+              return Tab(text: choice);
+            }).toList(),
+            isScrollable: true,
           ),
-          color: Colors.green,
-          onPressed: () {
-            Navigator.pop(context);
-          },
+        ),
+        body: TabBarView(
+          children: choices.map((String choice) {
+            return Center(
+              child: Text(
+                choice,
+                style: TextStyle(fontSize: 31),
+              ),
+            );
+          }).toList(),
         ),
       ),
     );
