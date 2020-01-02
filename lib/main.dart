d69045d897ac334a3ab13e5aaf96eab3077c6fd6
zeus <wpdn79@likelion.org>
2020-01-02 22:18:29 +0900

    탭바 기본( 아이콘 + 제목)

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,12 @@ void main() {
   runApp(MyApp());
 }
 
+class Choice {
+  Choice(this.text, this.icon);
+  final String text;
+  final IconData icon;
+}
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -12,7 +18,15 @@ class MyApp extends StatelessWidget {
 }
 
 class MainPage extends StatelessWidget {
-  final choices = ['PLANE', 'CAR', 'BIKE', 'BOAT', 'BUS', 'TRAIN', 'WALK'];
+  final choices = [
+    Choice('PLANE', Icons.flight),
+    Choice('CAR', Icons.directions_car),
+    Choice('BIKE', Icons.directions_bike),
+    Choice('BOAT', Icons.directions_boat),
+    Choice('BUS', Icons.directions_bus),
+    Choice('TRAIN', Icons.directions_railway),
+    Choice('WALK', Icons.directions_walk),
+  ];
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -21,23 +35,47 @@ class MainPage extends StatelessWidget {
         appBar: AppBar(
           title: Text('교통 수단'),
           bottom: TabBar(
-            tabs: choices.map((String choice) {
-              return Tab(text: choice);
+            tabs: choices.map((Choice choice) {
+              return Tab(
+                text: choice.text,
+                icon: Icon(choice.icon),
+              );
             }).toList(),
             isScrollable: true,
           ),
         ),
         body: TabBarView(
-          children: choices.map((String choice) {
-            return Center(
-              child: Text(
-                choice,
-                style: TextStyle(fontSize: 31),
-              ),
+          children: choices.map((Choice choice) {
+            return ChoiceCard(
+              text: choice.text,
+              icon: choice.icon,
             );
           }).toList(),
         ),
       ),
+    );
+  }
+}
+
+class ChoiceCard extends StatelessWidget {
+  const ChoiceCard({Key key, this.text, this.icon}) : super(key: key);
+
+  final String text;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    final TextStyle textStyle = Theme.of(context).textTheme.display3;
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Icon(icon, size: 128.0, color: textStyle.color),
+          Text(text, style: textStyle),
+        ],
+        mainAxisAlignment: MainAxisAlignment.center,
+      ),
+      color: Colors.green,
+      margin: EdgeInsets.all(12),
     );
   }
 }
