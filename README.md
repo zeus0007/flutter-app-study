e03b1b0ca72c47a3712dd50cf36fcf837dd8fd15
zeus <wpdn79@likelion.org>
2020-01-09 23:38:39 +0900

    README Update

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-flutter 공부내용 정리
-1. pykungBlog-flutter branch
+## flutter 공부내용 정리
+
+1. [pykungBlog-flutter branch](https://github.com/zeus0007/flutterapp_study/branches)
    - 파이쿵 블로그 플러터 공부 내용 정리
+  
 2. LearnFlutter&Dart-Udemy brnach
    - Udemy.com에서 Learn Flutter & Dart to Build iOS & Android Apps [2020]강의 내용 정리
---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,19 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<path_provider/PathProviderPlugin.h>)
+#import <path_provider/PathProviderPlugin.h>
+#else
+@import path_provider;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
+}
+
+@end
