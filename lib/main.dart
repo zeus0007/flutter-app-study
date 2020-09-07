603e56000a4d54d5045cd8d693322b9d4e3e2ca6
zeus0007 <wpdn79@likelion.org>
2020-09-07 18:43:15 +0900

    83. How to Play Sound Across Platforms

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -10,7 +10,13 @@ class XylophoneApp extends StatelessWidget {
       home: Scaffold(
         body: SafeArea(
           child: Center(
-            child: Text(nouns.last),
+            child: FlatButton(
+              onPressed: () {
+                final player = AudioCache();
+                player.play('note1.wav');
+              },
+              child: Text('Click Me'),
+            ),
           ),
         ),
       ),
