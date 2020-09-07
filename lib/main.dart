49e6c19866bb99d437454dac0aada7e2bb2c1c55
zeus0007 <wpdn79@likelion.org>
2020-09-07 18:53:42 +0900

    84. How to Play Multiple Sounds

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,59 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNum) {
+    final player = AudioCache();
+    player.play('note$soundNum.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note1.wav');
-              },
-              child: Text('Click Me'),
-            ),
+          child: Column(
+            children: <Widget>[
+              FlatButton(
+                onPressed: () {
+                  playSound(1);
+                },
+                color: Colors.red,
+              ),
+              FlatButton(
+                onPressed: () {
+                  playSound(2);
+                },
+                color: Colors.orange,
+              ),
+              FlatButton(
+                onPressed: () {
+                  playSound(3);
+                },
+                color: Colors.yellow,
+              ),
+              FlatButton(
+                onPressed: () {
+                  playSound(4);
+                },
+                color: Colors.green,
+              ),
+              FlatButton(
+                onPressed: () {
+                  playSound(5);
+                },
+                color: Colors.blueGrey,
+              ),
+              FlatButton(
+                  onPressed: () {
+                    playSound(6);
+                  },
+                  color: Colors.blue),
+              FlatButton(
+                  onPressed: () {
+                    playSound(7);
+                  },
+                  color: Colors.purple),
+            ],
           ),
         ),
       ),
