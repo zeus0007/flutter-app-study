c08e51863518717e749651dc430e4213f3fbc3b7
zeus0007 <wpdn79@likelion.org>
2020-09-13 18:38:38 +0900

    151. Getting location Data From Across Platforms

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,13 +7,21 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  void getLocation() async {
+    print('hello1');
+    Position position =
+        await getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+    print('hello2');
+    print(position);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: RaisedButton(
           onPressed: () {
-            //Get the current location
+            getLocation();
           },
           child: Text('Get Location'),
         ),
